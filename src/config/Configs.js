868f3131554e26d5c780c32162691d195710504d
Microsoft Graph DevX Tooling <timwamalwa@gmail.com>
2025-04-03 16:44:48 +0300

    Updated Api endpoint

--- src/config/Configs.js
+++ src/config/Configs.js
@@ -1,4 +1,4 @@
-const apiurl='https://pesapaygateway.com:9444/api/';
+const apiurl='https://sugarportal.sonysugar.co.ke:9444/api/';
 const gateway_url='http://localhost:8085/actuator/';
 export default{
     apiurl,
